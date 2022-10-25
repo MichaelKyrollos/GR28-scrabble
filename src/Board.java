baf9ba357d14904d8d4cb88b6b6c7d7a7c33b1a4
Pathum Danthanarayana <pathumkavindya@gmail.com>
2022-10-25 04:25:29 -0400

    Implemented overflow logic for Board class
    Added the following changes to the Board class:
    - Fixed the issue of letters being placed in the opposite direction (e.g. letters that should be placed horizontally were placed vertically)
    - Fixed the issue of the first letter being placed at the incorrect square on the board
    - Implemented overflow logic for the board so that false is returned if a single tile goes 'out of bounds', or outside of the board.
    - Implemented a 'save state' feature to the board, where the board saves a copy of its current state prior to placing tiles on its squares. If a single tile is out of bounds, the board reverts to its original state.
    
    Added the following changes to the Square class:
    - Implemented a second constructor for the Square class, which acts as a copy constructor to allow for the implementation of the 'save state' feature (creating a deep copy of an object) in the playWord method in the Board class.

--- src/Board.java
+++ src/Board.java
@@ -6,7 +6,7 @@ import java.util.*;
  * Squares contain a game Tile
  *
  * @author Michael Kyrollos, 101183521
- * @version 1.0
+ * @version 1.1
  * @date October 22, 2022
  */
 
@@ -30,6 +30,9 @@ public class Board {
      * Returns True if word has been placed on board by player, false otherwise.
      *
      * @author Michael Kyrollos, ID: 101183521
+     * @author Pathum Danthanarayana, 101181411
+     * @version 1.1
+     * @date October 22, 2022
      *
      * @return True if word has been placed, False otherwise.
      *
@@ -52,23 +55,131 @@ public class Board {
         column.put('N',13);
         column.put('O',14);
 
+        // Save the state of the board before placing any tiles
+        // (create a copy of the array of squares)
+        Square[][] savedSquares;
+        savedSquares = new Square[SIZE][SIZE];
+
+        // Traverse through each square on the current board
+        for (int i = 0; i < squares.length; i++)
+        {
+            for (int j = 0; j < squares[i].length; j++)
+            {
+                // Save a copy of the current square
+                savedSquares[i][j] = new Square(squares[i][j]);
+            }
+        }
+
+        // Store first character of command
         char firstChar = command.getThirdWord().charAt(0);
+        // Store second character of command
         char secondChar = command.getThirdWord().charAt(1);
 
-        if (isDigit(firstChar)){
-            int rows = Character.getNumericValue(firstChar);
-            int i =  column.get(secondChar);
-            for (int j = rows; j < tilesToPlay.size() + rows ; j++) {
-                    squares[j][i].placeSquare(tilesToPlay.get(j-rows));}
-            System.out.println(this);
-        }
-        else {
-            int j =  column.get(firstChar);
-            int columns = Character.getNumericValue(secondChar);
-            for (int i = columns; i < tilesToPlay.size() + columns ; i++) {
-                squares[j][i].placeSquare(tilesToPlay.get(i-columns));}
-            System.out.println(this);
+        // Check if the player has specified a double-digit row (e.g. 10, 11, 12, 13, 14)
+        if (command.getThirdWord().length() == 3)
+        {
+            // Store the third character of command
+            char thirdChar = command.getThirdWord().charAt(2);
+            // Check if the first character is an integer (e.g. player has entered 10A)
+            if (isDigit(firstChar))
+            {
+                //System.out.println("Printing horizontally for double-digit row!");
+                int rows = Character.getNumericValue(firstChar + secondChar) - 1;
+                int i = column.get(thirdChar);
+                for (int j = rows; j < tilesToPlay.size() + rows ; j++) {
+                    try
+                    {
+                        // Attempt to place the tile on the square
+                        squares[i][j].placeSquare(tilesToPlay.get(j-rows));
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Cannot place word. A letter has gone out of bounds.");
+                        // Reset the state of the board
+                        this.squares = savedSquares;
+
+                        return false;
+                    }
+                }
+                // Print out the updated board
+                System.out.println(this);
             }
+
+            // Otherwise, player has entered e.g. A10
+            else
+            {
+                //System.out.println("Printing vertically for double-digit row!");
+                int j =  column.get(firstChar);
+                int columns = Character.getNumericValue(secondChar + thirdChar) - 1;
+                for (int i = columns; i < tilesToPlay.size() + columns ; i++) {
+                    try
+                    {
+                        // Attempt to place the tile on the square
+                        squares[i][j].placeSquare(tilesToPlay.get(i-columns));
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Cannot place word. A letter has gone out of bounds.");
+                        // Reset the state of the board
+                        this.squares = savedSquares;
+
+                        return false;
+                    }
+                }
+                // Print the updated board
+                System.out.println(this);
+            }
+        }
+
+        // Otherwise, the player has specified a single-digit row (e.g. 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        else
+        {
+            // Check if the first character is an integer (e.g. player has entered 6A)
+            if (isDigit(firstChar)){
+                //System.out.println("Printing horizontally!");
+                int rows = Character.getNumericValue(firstChar) - 1;
+                int i = column.get(secondChar);
+                for (int j = rows; j < tilesToPlay.size() + rows ; j++) {
+                    try
+                    {
+                        // Attempt to place the tile on the square
+                        squares[i][j].placeSquare(tilesToPlay.get(j-rows));
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Cannot place word. A letter has gone out of bounds.");
+                        // Reset the state of the board
+                        this.squares = savedSquares;
+
+                        return false;
+                    }
+                }
+                // Print out the updated board
+                System.out.println(this);
+            }
+            else {
+                //System.out.println("Printing vertically!");
+                int j =  column.get(firstChar);
+                int columns = Character.getNumericValue(secondChar) - 1;
+                for (int i = columns; i < tilesToPlay.size() + columns ; i++) {
+                    try
+                    {
+                        // Attempt to place the tile on the square
+                        squares[i][j].placeSquare(tilesToPlay.get(i-columns));
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("Cannot place word. A letter has gone out of bounds.");
+                        // Reset the state of the board
+                        this.squares = savedSquares;
+
+                        return false;
+                    }
+                }
+                // Print out the updated board
+                System.out.println(this);
+            }
+        }
         return true;
     }
 
