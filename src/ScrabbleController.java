b53467a96cbd97137119e4acd2104e9c1e49305e
Chiller <yehandesilva@cmail.carleton.ca>
2022-11-17 10:48:43 -0500

    Refactored scrabble controller acording to TA feedback

--- src/ScrabbleController.java
+++ src/ScrabbleController.java
@@ -55,134 +55,229 @@ public class ScrabbleController implements ActionListener {
      * @author Yehan De Silva
      * @author Amin Zeina 101186297
      * @author Pathum Danthanarayana, 101181411
-     * @version 3.0
-     * @date November 14, 2022
+     * @version 3.1
+     * @date November 17, 2022
      */
     @Override
     public void actionPerformed(ActionEvent e) {
+        //If tile is selected
         if (e.getSource() instanceof Tile) {
             Tile tile = (Tile) e.getSource();
+            //Tile is to be played
             if (isPlaying) {
-                // Check if a tile is already selected
-                if (selectedTile != null)
-                {
-                    // If so, unselect the currently selected tile
-                    selectedTile.setBackground(Color.WHITE);
-                }
-                selectedTile = tile;
-
-                // Change background colour of the selected tile to show it has been selected
-                selectedTile.setBackground(ScrabbleFrameView.SELECTED_TILE_COLOR);
+                playTileSelected(tile);
             }
-            if (isRedrawing) {
-                //Add tile to list of tiles to redraw
-                if(!(tilesToRedraw.contains(tile))) {
-                    // Change background colour of the selected tile to show it has been selected
-                    tile.setBackground(ScrabbleFrameView.SELECTED_TILE_COLOR);
-                    tilesToRedraw.add(tile);
-                }
-                //Remove tile if it is already in the list of tiles to redraw
-                else {
-                    tilesToRedraw.remove(tile);
-                    tile.setBackground(Color.WHITE);
-                }
+            //Tile is to be redrawn
+            else if (isRedrawing) {
+                redrawTileSelected(tile);
             }
         }
-
+        //Square is to be played on
         if (e.getSource() instanceof Square) {
             Square square = (Square) e.getSource();
             if (isPlaying) {
-                if (square.getTile() == null && selectedTile != null) {
-                    // square is empty, so place the tile
-                    if (selectedTile instanceof BlankTile) {
-                        // blank tile has been selected, so set the letter to what the user wants
-                        BlankTile blankTile = (BlankTile) selectedTile;
-                        boolean isCharLetter = false;
-                        while (!isCharLetter) {
-                            String tileLetter = JOptionPane.showInputDialog(scrabbleFrame, "Enter the letter " +
-                                    "you'd like to use for this blank tile: ");
-                            if (tileLetter.length() == 1 && isAlphabetic(tileLetter.charAt(0))) {
-                                blankTile.setLetter(toUpperCase(tileLetter.charAt(0)));
-                                isCharLetter = true;
-                            } else {
-                                JOptionPane.showMessageDialog(scrabbleFrame, "That letter is invalid, try again");
-                            }
-                        }
-                    }
-                    selectedTile.setBackground(Color.WHITE);
-                    scrabbleModel.getCurrentPlayer().playTile(square, selectedTile);
-                    squaresInWord.add(square);
-                    tilesPlaced.add(selectedTile);
-                    selectedTile = null;
-                    square.setEnabled(false);
-                } else if (square.getTile() != null && selectedTile != null) {
-                    // Change colour of tile to show it is unselected
-                    selectedTile.setBackground(Color.WHITE);
-                    // square not empty -> unselect tile and prompt user
-                    selectedTile = null;
-                    JOptionPane.showMessageDialog(null, "That square is full. Tile unselected.");
-                } else if (square.getTile() != null && selectedTile == null){
-                    // square not empty but no tile selected -> add existing clicked square to word
-                    squaresInWord.add(square);
-                    square.setEnabled(false);
-                }
-                // otherwise, square empty and no tile selected, do nothing
+                playSquareSelected(square);
             }
         }
 
+        //Game start button clicked
         if (e.getActionCommand().equals("Let's play!")) {
-            configurePlayerInformation();
+            this.configurePlayerInformation();
         }
+        //Play button clicked
         else if (e.getActionCommand().equals("Play")) {
-            isPlaying = true;
-            scrabbleFrame.getRedrawButton().setEnabled(false);
-            scrabbleFrame.getSkipButton().setEnabled(false);
-            scrabbleModel.getGameBoard().copyBoardSquares();
-            JButton playButton = (JButton) e.getSource();
-            playButton.setText("Submit");
-            playButton.setBackground(ScrabbleFrameView.SELECTED_BUTTON_COLOR);
+            this.playSelected();
         }
+        //Redraw button clicked
         else if (e.getActionCommand().equals("Redraw")) {
-            this.isRedrawing = true;
-            scrabbleFrame.getPlayButton().setEnabled(false);
-            scrabbleFrame.getSkipButton().setEnabled(false);
-            JButton redrawButton = (JButton) e.getSource();
-            redrawButton.setText("Submit");
-            redrawButton.setBackground(ScrabbleFrameView.SELECTED_BUTTON_COLOR);
+            this.redrawSelected();
         }
+        //Skip button clicked
         else if (e.getActionCommand().equals("Skip")) {
             scrabbleModel.endTurn();
-        } else if (e.getActionCommand().equals("Submit")) {
+        }
+        //Submit button clicked
+        else if (e.getActionCommand().equals("Submit")) {
+            //Submit clicked while playing
             if (isPlaying) {
-                isPlaying = false;
-                scrabbleFrame.getRedrawButton().setEnabled(true);
-                scrabbleFrame.getSkipButton().setEnabled(true);
-                JButton submitButton = (JButton) e.getSource();
-                submitButton.setText("Play");
-                submitButton.setBackground(ScrabbleFrameView.ACCENT_COLOR);
-                scrabbleModel.playWord(new PlayWordEvent(scrabbleModel, squaresInWord, tilesPlaced));
-                for (Square square : squaresInWord) {
-                    square.setEnabled(true);
-                }
-                squaresInWord.clear();
-                tilesPlaced.clear();
-                tilesToRedraw.clear();
+                submitPlaySelected();
             }
+            //Submit clicked while redrawing
             else if (isRedrawing) {
-                isRedrawing = false;
-                scrabbleFrame.getPlayButton().setEnabled(true);
-                scrabbleFrame.getSkipButton().setEnabled(true);
-                JButton submitButton = (JButton) e.getSource();
-                submitButton.setText("Redraw");
-                submitButton.setBackground(ScrabbleFrameView.ACCENT_COLOR);
-                this.resetRedrawnTilesColour();
-                scrabbleModel.redraw(tilesToRedraw);
-                tilesToRedraw.clear();
+                submitRedrawSelected();
             }
         }
-        if (e.getActionCommand().equals("Quit Game")) {
+        //Quit game selected
+        else if (e.getActionCommand().equals("Quit Game")) {
             scrabbleModel.endGame();
         }
+    }
+
+    /**
+     * Plays the currently selected tile if there is one in the given square.
+     * @param square Square the selected tile is to be placed on
+     *
+     * @author Yehan De Silva
+     * @version 3.0
+     * @date November 17, 2022
+     */
+    private void playSquareSelected(Square square) {
+        if (square.getTile() == null && selectedTile != null) {
+            // square is empty, so place the tile
+            if (selectedTile instanceof BlankTile) {
+                // blank tile has been selected, so set the letter to what the user wants
+                BlankTile blankTile = (BlankTile) selectedTile;
+                boolean isCharLetter = false;
+                while (!isCharLetter) {
+                    String tileLetter = JOptionPane.showInputDialog(scrabbleFrame, "Enter the letter " +
+                            "you'd like to use for this blank tile: ");
+                    if (tileLetter.length() == 1 && isAlphabetic(tileLetter.charAt(0))) {
+                        blankTile.setLetter(toUpperCase(tileLetter.charAt(0)));
+                        isCharLetter = true;
+                    } else {
+                        JOptionPane.showMessageDialog(scrabbleFrame, "That letter is invalid, try again");
+                    }
+                }
+            }
+            selectedTile.setBackground(Color.WHITE);
+            scrabbleModel.getCurrentPlayer().playTile(square, selectedTile);
+            squaresInWord.add(square);
+            tilesPlaced.add(selectedTile);
+            selectedTile = null;
+            square.setEnabled(false);
+        } else if (square.getTile() != null && selectedTile != null) {
+            // Change colour of tile to show it is unselected
+            selectedTile.setBackground(Color.WHITE);
+            // square not empty -> unselect tile and prompt user
+            selectedTile = null;
+            JOptionPane.showMessageDialog(null, "That square is full. Tile unselected.");
+        } else if (square.getTile() != null && selectedTile == null){
+            // square not empty but no tile selected -> add existing clicked square to word
+            squaresInWord.add(square);
+            square.setEnabled(false);
+        }
+        // otherwise, square empty and no tile selected, do nothing
+    }
+
+    /**
+     * Adds the given tile to the list of tiles to be redrawn or removes it if it already exists.
+     * @param tile Tile to be added/removed from the redrawn tiles list.
+     *
+     * @author Yehan De Silva
+     * @version 3.0
+     * @date November 17, 2022
+     */
+    private void redrawTileSelected(Tile tile) {
+        //Add tile to list of tiles to redraw
+        if(!(tilesToRedraw.contains(tile))) {
+            // Change background colour of the selected tile to show it has been selected
+            tile.setBackground(ScrabbleFrameView.SELECTED_TILE_COLOR);
+            tilesToRedraw.add(tile);
+        }
+        //Remove tile if it is already in the list of tiles to redraw
+        else {
+            tilesToRedraw.remove(tile);
+            tile.setBackground(Color.WHITE);
+        }
+    }
+
+    /**
+     * Sets the given tile as the selected tile.
+     * @param tile Tile to be set as the selected tile.
+     *
+     * @author Yehan De Silva
+     * @version 3.0
+     * @date November 17, 2022
+     */
+    private void playTileSelected(Tile tile) {
+        // Check if a tile is already selected
+        if (selectedTile != null)
+        {
+            // If so, unselect the currently selected tile
+            selectedTile.setBackground(Color.WHITE);
+        }
+        selectedTile = tile;
+
+        // Change background colour of the selected tile to show it has been selected
+        selectedTile.setBackground(ScrabbleFrameView.SELECTED_TILE_COLOR);
+    }
+
+    /**
+     * Redraws the list of the tiles to be redrawn.
+     *
+     * @author Yehan De Silva
+     * @version 3.0
+     * @date November 17, 2022
+     */
+    private void submitRedrawSelected() {
+        //Redrawing is submitted, so change button views
+        isRedrawing = false;
+        scrabbleFrame.getPlayButton().setEnabled(true);
+        scrabbleFrame.getSkipButton().setEnabled(true);
+        scrabbleFrame.getRedrawButton().setText("Redraw");
+        scrabbleFrame.getRedrawButton().setBackground(ScrabbleFrameView.ACCENT_COLOR);
+        //Only perform redraw method in model if at least one tile is selected to redraw
+        if (!tilesToRedraw.isEmpty()) {
+            this.resetRedrawnTilesColour();
+            scrabbleModel.redraw(tilesToRedraw);
+            tilesToRedraw.clear();
+        }
+    }
+
+    /**
+     * Attempts to play the user created word on the board.
+     *
+     * @author Yehan De Silva
+     * @version 3.0
+     * @date November 17, 2022
+     */
+    private void submitPlaySelected() {
+        //Playing is submitted, so change button views
+        isPlaying = false;
+        scrabbleFrame.getRedrawButton().setEnabled(true);
+        scrabbleFrame.getSkipButton().setEnabled(true);
+        scrabbleFrame.getPlayButton().setText("Play");
+        scrabbleFrame.getPlayButton().setBackground(ScrabbleFrameView.ACCENT_COLOR);
+        scrabbleModel.playWord(new PlayWordEvent(scrabbleModel, squaresInWord, tilesPlaced));
+        for (Square square : squaresInWord) {
+            square.setEnabled(true);
+        }
+        squaresInWord.clear();
+        tilesPlaced.clear();
+        tilesToRedraw.clear();
+    }
+
+    /**
+     * Updates the controller state and game views to the play status.
+     *
+     * @author Yehan De Silva
+     * @version 3.0
+     * @date November 17, 2022
+     */
+    private void playSelected() {
+        //Update state and views if play is selected
+        isPlaying = true;
+        scrabbleFrame.getRedrawButton().setEnabled(false);
+        scrabbleFrame.getSkipButton().setEnabled(false);
+        scrabbleModel.getGameBoard().copyBoardSquares();
+        scrabbleFrame.getPlayButton().setText("Submit");
+        scrabbleFrame.getPlayButton().setBackground(ScrabbleFrameView.SELECTED_BUTTON_COLOR);
+    }
+
+    /**
+     * Updates the controller state and game views to the redraw status.
+     *
+     * @author Yehan De Silva
+     * @version 3.0
+     * @date November 17, 2022
+     */
+    private void redrawSelected() {
+        //Update state and views if redraw is selected
+        this.isRedrawing = true;
+        scrabbleFrame.getPlayButton().setEnabled(false);
+        scrabbleFrame.getSkipButton().setEnabled(false);
+        scrabbleFrame.getRedrawButton().setText("Submit");
+        scrabbleFrame.getRedrawButton().setBackground(ScrabbleFrameView.SELECTED_BUTTON_COLOR);
     }
 
     /**
